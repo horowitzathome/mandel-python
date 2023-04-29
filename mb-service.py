c1e5d16b6ac0739d4a1522e1a52cbafe1ad8cea6
horowitzathome <georg.nozicka@gmail.com>
2023-04-29 07:23:23 +0200

    Adapted Dockerfile; port 8080

--- mb-service.py
+++ mb-service.py
@@ -8,7 +8,7 @@ import app
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Mandelbrot Service')
     parser.add_argument('--host', default='0.0.0.0', help='Host to bind the service (default: 0.0.0.0)')
-    parser.add_argument('--port', type=int, default=8000, help='Port to bind the service (default: 8000)')
+    parser.add_argument('--port', type=int, default=8080, help='Port to bind the service (default: 8000)')
     parser.add_argument('--worker-threads', type=int, default=4, help='Number of worker threads (default: 4)')
     return parser.parse_args()
 
